f3ed07f18c5694740991ec3b686a3d0898b39d08
lixiaolongxl <lxldevtest@163.com>
2022-01-20 11:25:12 +0800

    mians

--- example/index.js
+++ example/index.js
@@ -1,3 +1,4 @@
-console.log(Object.prototype.toString.call('1'));
-console.log(Object.prototype.toString.call([]));
+// console.log(Object.prototype.toString.call('1'));
+// console.log(Object.prototype.toString.call([]));
+    console.log(String);
     
